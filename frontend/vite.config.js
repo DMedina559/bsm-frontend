c0946d32ae775cd305345eb8c05c9a88a2f089ec
Danny <68480549+DMedina559@users.noreply.github.com>
2026-06-23 11:27:00 -0700

    build: show clean frontend ver

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,7 @@
 /* global process */
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
-import { execSync } from "child_process";
+import packageJson from "./package.json" with { type: "json" };
 
 // Custom plugin to redirect /app to /app/
 const redirectApp = () => ({
@@ -28,14 +28,7 @@ export default defineConfig(({ mode }) => {
     ? target.replace("https", "wss")
     : target.replace("http", "ws");
 
-  let appVersion = "unknown";
-  try {
-    appVersion = execSync("git describe --tags --always --dirty")
-      .toString()
-      .trim();
-  } catch (e) {
-    console.warn("Failed to get git version", e);
-  }
+  let appVersion = packageJson.version || "unknown";
 
   return {
     plugins: [react(), redirectApp()],
